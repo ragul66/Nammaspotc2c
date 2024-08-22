57f25f3db0e28eaeaa6751d29632749fdd3b3393
ragul66 <ragul1.gokul2.3@gmail.com>
2024-08-22 10:48:10 +0530

    navbar changes

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 
 function Menu() {
+  const data = [
+    {
+      heading: "",
+      img: "",
+      headdname: "",
+      Description: "",
+      price: "₹99",
+    },
+  ];
   return (
     <>
       <div className="flex justify-center items-center font-font1 text-3xl font-bold mt-12">
         our Home
+      </div>
+
+      <div className="flex flex-row justify-center items-center space-x-2 mt-12">
+        <p className="text-white bg-cardbg p-2 rounded-full w-20 flex justify-center items-center">
+          All
+        </p>
+        <p className="text-white bg-cardbg p-2 rounded-full w-20 flex justify-center items-center">
+          Burger
+        </p>
+        <p className="text-white bg-cardbg p-2 rounded-full w-20 flex justify-center items-center">
+          Pizza
+        </p>
+        <p className="text-white bg-cardbg p-2 rounded-full w-20 flex justify-center items-center">
+          Pasta
+        </p>
+        <p className="text-white bg-cardbg p-2 rounded-full w-20 flex justify-center items-center">
+          Fries
+        </p>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,14 @@ import logo from "../assets/logo.jpg";
 function Navbar() {
   return (
     <div className="absolute flex flex-row justify-center items-center">
-      <div className="flex flex-col lg:p-4 p-4">
+      <div className="flex flex-row lg:p-4 p-4 space-x-2">
         <img
-          className="w-16 h-16 rounded-full md:ml-[130px] lg:ml-[200px] mt-2 border-2 border-white"
+          className="w-16 h-16 rounded-full md:ml-[130px] lg:ml-[200px] mt-2 border-2 border-white "
           src={logo}
         />
+        <p className="flex justify-center items-center font-font1 lg:text-2xl text-white">
+          NammaSpotc2c
+        </p>
       </div>
       {/* navbar pages */}
       <div className="hidden sm:flex flex-row space-x-5 items-center justify-center font-font1 text-white  cursor-pointer">
