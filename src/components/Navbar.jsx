7de5e6d044e401c242da0e16766e79743cc1b632
ragul66 <ragul1.gokul2.3@gmail.com>
2024-08-26 23:30:52 +0530

    .../

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,63 @@
 // import React, { useState } from "react";
 // import logo from "../assets/logo.jpg";
 // import gif from "../assets/special gif.gif";
+// import combooffer from "../assets/combooffer.png";
+// import burger from "../assets/pasta.png";
 // import { useNavigate } from "react-router-dom";
 
 // function Navbar() {
 //   const [selectedCategory, setSelectedCategory] = useState("HOME");
+//   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State for mobile menu
 //   const navigate = useNavigate();
 
 //   const handleNavigation = (category, path) => {
 //     setSelectedCategory(category);
 //     navigate(path);
+//     setIsMobileMenuOpen(false); // Close mobile menu after navigation
+//   };
+
+//   const toggleMobileMenu = () => {
+//     setIsMobileMenuOpen(!isMobileMenuOpen);
 //   };
 
 //   return (
-//     <div className="absolute flex flex-col sm:flex-row items-center sm:justify-center w-full p-4">
+//     <div className=" flex flex-col sm:flex-row items-center  w-full  p-4  bg-yellow-100  lg:bg-yellow-100 ">
 //       {/* Logo and Brand Name */}
-//       <div className="flex items-center space-x-2 w-full lg:ml-[100px] sm:w-auto justify-start sm:justify-center">
+//       <div className="flex items-center space-x-2 w-full lg:ml-[100px] sm:w-auto justify-between sm:justify-center">
 //         <img
 //           className="w-16 h-16 rounded-full mt-2 border-2 border-white"
 //           src={logo}
 //           alt="Logo"
 //         />
-//         <p className=" lg:text-2xl text-xl text-white font-bold font-font1">
+//         <p className="lg:text-2xl text-xl text-black font-bold  font-serif">
 //           NammaSpotc2c
 //         </p>
+
+//         {/* Hamburger Menu for Mobile */}
+//         <div className="sm:hidden">
+//           <button
+//             onClick={toggleMobileMenu}
+//             className="text-white focus:outline-none"
+//           >
+//             <img
+//               name={isMobileMenuOpen ? "close-outline" : "menu-outline"}
+//               src={burger}
+//             />
+//             {/* <i class="fa-solid fa-bars"></i> */}
+//           </button>
+//         </div>
 //       </div>
 
-//       {/* Navbar Pages - Hidden on mobile */}
-//       <div className="hidden sm:flex flex-row space-x-10 items-center justify-center font-serif text-white cursor-pointer flex-grow">
+//       {/* Navbar Pages - Shown based on screen size */}
+//       <div
+//         className={`${
+//           isMobileMenuOpen ? "flex" : "hidden"
+//         } sm:flex flex-col sm:flex-row space-y-4 sm:space-y-0 space-x-0 sm:space-x-10 items-center justify-center font-serif text-black cursor-pointer flex-grow`}
+//       >
 //         <a
 //           className={`hover:text-buttoncl ${
 //             selectedCategory === "HOME"
-//               ? "text-gold  border-b-2 border-buttoncl"
+//               ? "text-red-600 border-b-2 border-red-600"
 //               : ""
 //           }`}
 //           onClick={() => handleNavigation("HOME", "/")}
@@ -40,7 +66,9 @@
 //         </a>
 //         <a
 //           className={`hover:text-buttoncl ${
-//             selectedCategory === "MENU" ? "text-gold" : ""
+//             selectedCategory === "MENU"
+//               ? "text-red-600 border-b-2 border-red-600"
+//               : ""
 //           }`}
 //           onClick={() => handleNavigation("MENU", "/Menu")}
 //         >
@@ -48,7 +76,9 @@
 //         </a>
 //         <a
 //           className={`hover:text-buttoncl ${
-//             selectedCategory === "ABOUT" ? "text-gold" : ""
+//             selectedCategory === "ABOUT"
+//               ? "text-red-600 border-b-2 border-red-600"
+//               : ""
 //           }`}
 //           onClick={() => handleNavigation("ABOUT", "/about")}
 //         >
@@ -56,37 +86,44 @@
 //         </a>
 //         <a
 //           className={`hover:text-buttoncl ${
-//             selectedCategory === "CONTACT" ? "text-gold" : ""
+//             selectedCategory === "CONTACT"
+//               ? "text-red-600 border-b-2 border-red-600"
+//               : ""
 //           }`}
 //           onClick={() => handleNavigation("CONTACT", "/Contact")}
 //         >
 //           CONTACT
 //         </a>
+//         <a
+//           className={` lg:w-20 lg:h-20 w-12 h-12 ${
+//             selectedCategory === "combos"
+//               ? "text-buttoncl border-b-2 border-buttoncl"
+//               : ""
+//           }`}
+//           onClick={() => handleNavigation("combos", "/combos")}
+//         >
+//           <img src={combooffer} />
+//         </a>
 //       </div>
 
+//       {/* Special Offer Icon */}
 //       <div className="sm:flex sm:flex-row mr-auto">
 //         <img
-//           className="w-12 h-12 md:w-20 md:h-20 lg:w-20 lg:h-20 -auto cursor-pointer animate-wiggle"
+//           className="hidden sm:block   w-12 h-12 md:w-20 md:h-20 lg:w-20 lg:h-20 cursor-pointer animate-wiggle"
 //           src={gif}
 //           onClick={() => navigate("/combos")}
+//           alt="Special Offer"
 //         />
 //       </div>
-
-//       {/* User Icon */}
-//       {/* <div className="text-white mt-2 sm:mt-0 sm:ml-auto">
-//         <ion-icon name="person-outline"></ion-icon>
-//       </div> */}
 //     </div>
 //   );
 // }
 
 // export default Navbar;
-
 import React, { useState } from "react";
 import logo from "../assets/logo.jpg";
 import gif from "../assets/special gif.gif";
 import combooffer from "../assets/combooffer.png";
-import burger from "../assets/pasta.png";
 import { useNavigate } from "react-router-dom";
 
 function Navbar() {
@@ -105,7 +142,7 @@ function Navbar() {
   };
 
   return (
-    <div className=" flex flex-col sm:flex-row items-center  w-full  p-4  bg-yellow-200  lg:bg-gray-500 ">
+    <div className="flex flex-col sm:flex-row items-center w-full p-4 bg-yellow-100 lg:bg-gray-500">
       {/* Logo and Brand Name */}
       <div className="flex items-center space-x-2 w-full lg:ml-[100px] sm:w-auto justify-between sm:justify-center">
         <img
@@ -113,7 +150,7 @@ function Navbar() {
           src={logo}
           alt="Logo"
         />
-        <p className="lg:text-2xl text-xl text-black font-bold  font-serif">
+        <p className="lg:text-2xl text-xl text-black font-bold font-serif">
           NammaSpotc2c
         </p>
 
@@ -121,13 +158,12 @@ function Navbar() {
         <div className="sm:hidden">
           <button
             onClick={toggleMobileMenu}
-            className="text-white focus:outline-none"
+            className="text-black focus:outline-none"
           >
-            <img
+            <ion-icon
               name={isMobileMenuOpen ? "close-outline" : "menu-outline"}
-              src={burger}
-            />
-            {/* <i class="fa-solid fa-bars"></i> */}
+              size="large"
+            ></ion-icon>
           </button>
         </div>
       </div>
@@ -179,21 +215,21 @@ function Navbar() {
           CONTACT
         </a>
         <a
-          className={` lg:w-20 lg:h-20 w-12 h-12 ${
+          className={`lg:w-20 lg:h-20 w-12 h-12 ${
             selectedCategory === "combos"
               ? "text-buttoncl border-b-2 border-buttoncl"
               : ""
           }`}
           onClick={() => handleNavigation("combos", "/combos")}
         >
-          <img src={combooffer} />
+          <img src={combooffer} alt="Combo Offer" />
         </a>
       </div>
 
       {/* Special Offer Icon */}
       <div className="sm:flex sm:flex-row mr-auto">
         <img
-          className="hidden sm:block   w-12 h-12 md:w-20 md:h-20 lg:w-20 lg:h-20 cursor-pointer animate-wiggle"
+          className="hidden sm:block w-12 h-12 md:w-20 md:h-20 lg:w-20 lg:h-20 cursor-pointer animate-wiggle"
           src={gif}
           onClick={() => navigate("/combos")}
           alt="Special Offer"
