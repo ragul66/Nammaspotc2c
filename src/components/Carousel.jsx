4a4201c110d0302a433f1090358e4021722e9bb1
ragul66 <ragul1.gokul2.3@gmail.com>
2024-08-27 12:49:19 +0530

    ....last changes in photo

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,6 +2,10 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import bannerl from "../assets/banner1.jpg";
+import bannerl2 from "../assets/bannerl2.jpg";
+
 import bannerm from "../assets/banner mobile.jpg";
 import bannerm1 from "../assets/pizzabannerm.jpg";
 import bannerm2 from "../assets/bannerm2.jpg";
@@ -21,10 +25,7 @@ const CarouselComponent = () => {
         <div className="w-full h-full">
           <picture>
             {/* Image for large screens */}
-            <source
-              media="(min-width: 1024px)"
-              srcSet="https://via.placeholder.com/1600x900.png?text=Large+Screen+Image+1"
-            />
+            <source media="(min-width: 1024px)" srcSet={bannerl} />
             {/* Image for smaller screens */}
             <img
               src={bannerm}
@@ -33,10 +34,10 @@ const CarouselComponent = () => {
             />
           </picture>
           <div className="absolute inset-0 flex flex-col justify-center items-center text-white bg-black bg-opacity-50">
-            <h2 className="text-2xl md:text-4xl  font-bold mb-4">
+            <h2 className="text-2xl md:text-4xl  font-bold mb-4 animate-left-to-right">
               Blasting Burgers
             </h2>
-            <p className="text-md md:text-lg p-2">
+            <p className="text-md md:text-lg p-2 animate-bottom-to-top">
               Burgers are sandwiches with a meat or veggie patty between buns.
               They often include toppings like cheese,
               <br /> lettuce, tomatoes, and sauces. Popular for their juicy
@@ -48,10 +49,7 @@ const CarouselComponent = () => {
         {/* Slide 2 */}
         <div className="w-full h-full">
           <picture>
-            <source
-              media="(min-width: 1024px)"
-              srcSet="https://via.placeholder.com/1600x900.png?text=Large+Screen+Image+2"
-            />
+            <source media="(min-width: 1024px)" srcSet={bannerl2} />
             <img
               src={bannerm1}
               alt="Slide 2"
@@ -59,10 +57,10 @@ const CarouselComponent = () => {
             />
           </picture>
           <div className="absolute inset-0 flex flex-col justify-center items-center text-white bg-black bg-opacity-50">
-            <h2 className="text-2xl md:text-4xl font-bold mb-4">
+            <h2 className="text-2xl md:text-4xl font-bold mb-4 animate-left-to-right">
               Pizza days💕
             </h2>
-            <p className="text-md md:text-lg p-2">
+            <p className="text-md md:text-lg p-2 animate-bottom-to-top">
               Pizza is a flatbread topped with tomato sauce, cheese, and various
               toppings like pepperoni, <br />
               vegetables, and olives. It is baked until crispy and is popular
@@ -85,10 +83,10 @@ const CarouselComponent = () => {
             />
           </picture>
           <div className="absolute inset-0 flex flex-col justify-center items-center text-white bg-black bg-opacity-50">
-            <h2 className="text-2xl md:text-4xl font-bold mb-4">
+            <h2 className="text-2xl md:text-4xl font-bold mb-4 animate-left-to-right">
               ASpiring Combos
             </h2>
-            <p className="text-md md:text-lg">
+            <p className="text-md md:text-lg animate-bottom-to-top">
               This is the third slide. Make it engaging!
             </p>
           </div>
